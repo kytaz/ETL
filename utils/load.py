6ab0d7f231dafb594ebb7974b9cde32a4863a105
Tazky Khumaira Tsany <163818093+kytaz@users.noreply.github.com>
2025-05-19 22:34:07 +0700

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -6,7 +6,7 @@ def save_to_csv(df, filename='products.csv'):
     df.to_csv(filename, index=False)
 
 # Baris ini yang diperbaiki: menambahkan tanda kurung () setelah nama fungsi
-def save_to_google_sheets(df, json_keyfile='etl-fashion-data-ee489a18d56a.json', sheet_url='https://docs.google.com/spreadsheets/d/1D2k3gybmOADPQOeXZfOi_xnLNaLrkA1fErpLVoGteLU/edit?usp=sharing'):
+def save_to_google_sheets(df, json_keyfile='project-etl-460308-8ae7b5cb6416.json', sheet_url='https://docs.google.com/spreadsheets/d/1D2k3gybmOADPQOeXZfOi_xnLNaLrkA1fErpLVoGteLU/edit?usp=sharing'):
     gc = gspread.service_account(filename=json_keyfile)
     sh = gc.open_by_url(sheet_url)
     worksheet = sh.sheet1
