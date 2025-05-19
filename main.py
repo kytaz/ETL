9d6b6b376758093ec9b7863eff63b8e8a51aba57
Tazky Khumaira Tsany <163818093+kytaz@users.noreply.github.com>
2025-05-19 15:46:25 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils.extract import fetch_products
+from utils.transform import process_data
+from utils.load import save_to_csv, save_to_google_sheets
+
+
+def main():
+    raw_data = fetch_products(pages=50)
+    df = process_data(raw_data)
+    save_to_csv(df)
+    save_to_google_sheets(df, json_keyfile='etl-fashion-data-ee489a18d56a.json')
+
+if __name__ == "__main__":
+    main()
