a5b6c6eca317e4254c884783f8d041f9e107f9d0
Tazky Khumaira Tsany <163818093+kytaz@users.noreply.github.com>
2025-05-20 14:32:21 +0700

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -1,11 +1,16 @@
+import pandas as pd
 import gspread
 from gspread_dataframe import set_with_dataframe
 
-# ... fungsi lain ...
+def save_to_csv(df, filename='products.csv'):
+    try:
+        df.to_csv(filename, index=False)
+    except Exception as e:
+        print(f"[ERROR] Gagal menyimpan ke CSV: {e}")
 
 def save_to_google_sheets(df, json_keyfile='project-etl-460308-464de4f7c024.json', sheet_url='https://docs.google.com/spreadsheets/d/1Vy4rViij8jDSrV6JoEEz0Whcl-QVN8X3l0_lMlYM85A/edit?usp=sharing'):
     try:
-        gc = gspread.service_account(filename=json_keyfile) # <-- Baris ini yang melakukan otentikasi
+        gc = gspread.service_account(filename=json_keyfile)
         sh = gc.open_by_url(sheet_url)
         worksheet = sh.sheet1
         worksheet.clear()
