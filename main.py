97a5b9af39a178a11bf45c1d10dd7a111e5ce224
Tazky Khumaira Tsany <163818093+kytaz@users.noreply.github.com>
2025-05-20 12:48:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from utils.extract import fetch_products
-from utils.transform import process_data
-from utils.load import save_to_csv, save_to_google_sheets
-
+from utils.utils_extract import fetch_products
+from utils.utils_transform import transform
+from utils.utils_load import save_to_csv, save_to_google_sheets
 
 def main():
     raw_data = fetch_products(pages=50)
-    df = process_data(raw_data)
+    df = transform(raw_data)
+    print("✅ Data berhasil ditransformasi. Kolom:", df.columns.tolist())
+    print("🧾 Contoh data:\n", df.head())
     save_to_csv(df)
     save_to_google_sheets(df, json_keyfile='project-etl-460308-8ae7b5cb6416.json')
 
